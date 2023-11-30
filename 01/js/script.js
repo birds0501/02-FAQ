537b29891c20b0ee551100af8f35949873d5b0b6
birds0501 <birds0501@naver.com>
2023-11-30 11:06:26 +0900

    faq toggle방식으로 변경, 디자인 수정

--- 01/js/script.js
+++ 01/js/script.js
@@ -7,6 +7,9 @@ $(function () {
   //질문을 클릭했을 때
   $faqQ.on("click", function () {
     //답변을 보여주자(slide down)
-    $(this).find($faqA).stop().slideDown(duration); //stop을 걸어놓으면 이상하게 덜덜거리는거 방지할 수 있음
+    $(this).find($faqA).stop().slideToggle(duration); //stop을 걸어놓으면 이상하게 덜덜거리는거 방지할 수 있음
+
+    //클릭 된 li에게 on클래스 부여
+    $(this).toggleClass("on");
   });
 });
